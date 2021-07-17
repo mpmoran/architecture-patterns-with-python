f1b10323ad96b036e33b7912a4bcdf2156836152
Michael P. Moran <30704827+mpmoran@users.noreply.github.com>
2021-07-17 12:35:06 -0500

    split allocate tests from batch tests

--- test_batches.py
+++ test_batches.py
@@ -5,8 +5,6 @@ import pytest
 from model import Batch, OrderLine, allocate, OutOfStock
 
 
-# from model import ...
-
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
@@ -60,44 +58,6 @@ def test_allocations_is_idempotent():
     assert batch.available_quantity == 18
 
 
-def test_prefers_current_stock_batches_to_shipments():
-    in_stock_batch = Batch('in-stock-batch', 'RETRO-CLOCK', 100, eta=None)
-    shipment_batch = Batch('shipment-batch', 'RETRO-CLOCK', 100, eta=tomorrow)
-    line = OrderLine('oref', 'RETRO-CLOCK', 10)
-
-    allocate(line, [in_stock_batch, shipment_batch])
-
-    assert in_stock_batch.available_quantity == 90
-    assert shipment_batch.available_quantity == 100
-
-
-def test_prefers_earlier_batches():
-    earliest = Batch('speedy-batch', 'MINIMALIST-SPOON', 100, eta=today)
-    medium = Batch('normal-batch', 'MINIMALIST-SPOON', 100, eta=tomorrow)
-    latest = Batch('slow-batch', 'MINIMALIST-SPOON', 100, eta=later)
-    line = OrderLine('order1', 'MINIMALIST-SPOON', 10)
-
-    allocate(line, [medium, earliest, latest])
-
-    assert earliest.available_quantity == 90
-    assert medium.available_quantity == 100
-    assert latest.available_quantity == 100
-
-
-def test_returns_allocated_batch_ref():
-    in_stock_batch = Batch('in-stock-batch-ref',
-                           'HIGHBROW-POSTER',
-                           100,
-                           eta=None)
-    shipment_batch = Batch('shipment-batch-ref',
-                           'HIGHBROW-POSTER',
-                           100,
-                           eta=tomorrow)
-    line = OrderLine('oref', 'HIGHBROW-POSTER', 10)
-    allocation = allocate(line, [in_stock_batch, shipment_batch])
-    assert allocation == in_stock_batch.reference
-
-
 def test_cannot_allocate_same_line_more_than_once():
     batch = Batch('batch-003', 'BLUE-VASE', qty=10, eta=date.today())
     line = OrderLine('order-ref', 'BLUE-VASE', 2)
@@ -106,11 +66,3 @@ def test_cannot_allocate_same_line_more_than_once():
     batch.allocate(line)
 
     assert batch.available_quantity == 8
-
-
-def test_raises_out_of_stock_exception_if_cannot_allocate():
-    batch, line1 = make_batch_and_line('SMALL-FORK', 10, 10)
-    allocate(line1, [batch])
-
-    with pytest.raises(OutOfStock, match='SMALL-FORK'):
-        allocate(OrderLine('order2', 'SMALL-FORK', 1), [batch])
